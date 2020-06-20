d3be7f8a885a67cc1a30f4e5282844d5e9e069df
lm <super1742720898@gmail.com>
2020-06-20 16:51:31 +0800

    :smile:modified team page

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -4,23 +4,64 @@
         <div class="cards">
             <transition appear
                         appear-active-class="animated flipInX">
-                <a class="card"></a>
+                <a class="card card1"
+                   :class="{'light-card':!isDarkMode,
+                   'dark-card':isDarkMode}">
+                    <img src="@/assets/slack.png" class="card-header"
+                         :class="{'light-header':!isDarkMode,
+                         'dark-header':isDarkMode}">
+                    <h3 :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Slack</h3>
+                    <p :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Messaging app for all team
+                        communication.</p>
+                </a>
             </transition>
             <transition appear
                         appear-active-class="animated flipInX">
-                <a class="card"></a>
+                <a class="card card2"
+                   :class="{'light-card':!isDarkMode,
+                   'dark-card':isDarkMode}">
+                    <img src="@/assets/notion.png" class="card-header"
+                         :class="{'light-header':!isDarkMode,
+                         'dark-header':isDarkMode}">
+                    <h3 :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Notion</h3>
+                    <p :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">All-in-one workspace.</p>
+                </a>
             </transition>
             <transition appear
                         appear-active-class="animated flipInX">
-                <a class="card"></a>
+                <a class="card card3"
+                   :class="{'light-card':!isDarkMode,
+                   'dark-card':isDarkMode}">
+                    <img src="@/assets/figma.png" class="card-header"
+                         :class="{'light-header':!isDarkMode,
+                         'dark-header':isDarkMode}">
+                    <h3 :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Figma</h3>
+                    <p :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Collaborative design tool.</p>
+                </a>
             </transition>
             <transition appear
                         appear-active-class="animated flipInX">
-                <a class="card"></a>
+                <a class="card card4"
+                   :class="{'light-card':!isDarkMode,
+                   'dark-card':isDarkMode}">
+                    <img src="@/assets/contentful.png" class="card-header"
+                         :class="{'light-header':!isDarkMode,
+                         'dark-header':isDarkMode}">
+                    <h3 :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Contentful</h3>
+                    <p :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Content management system.</p>
+                </a>
             </transition>
             <transition appear
                         appear-active-class="animated flipInX">
-                <a class="card"></a>
+                <a class="card card5"
+                   :class="{'light-card':!isDarkMode,
+                   'dark-card':isDarkMode}">
+                    <img src="@/assets/dropbox.png" class="card-header"
+                         :class="{'light-header':!isDarkMode,
+                         'dark-header':isDarkMode}">
+                    <h3 :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Dropbox</h3>
+                    <p :class="{'light-text':isDarkMode,'dark-text':!isDarkMode}">Storage space in the cloud.</p>
+                </a>
             </transition>
         </div>
     </div>
@@ -29,7 +70,12 @@
 
 <script>
     export default {
-        name: "About"
+        name: "About",
+        computed: {
+            isDarkMode() {
+                return this.$store.getters.isDarkMode;
+            }
+        }
     }
 </script>
 
@@ -50,10 +96,76 @@
     }
 
     .card {
-        height: 50px;
-        width: 50px;
-        background: red;
+        width: 100%;
+        max-width: 300px;
+        height: 400px;
+        border-radius: 10px;
         margin: 20px;
+    }
+
+    .card1 {
+        animation-delay: 0;
+    }
+
+    .card2 {
+        animation-delay: 0.5s;
+    }
+
+    .card3 {
+        animation-delay: 1s;
+    }
+
+    .card4 {
+        animation-delay: 1.5s;
+    }
+
+    .card5 {
+        animation-delay: 2s;
+    }
+
+    .card-header {
+        width: 100%;
+        border-radius: 10px 10px 0 0;
+    }
+
+    .light-header {
+        background: $light-gray;
+    }
+
+    .dark-header {
+        background: #283049;
+    }
+
+    h3 {
+        @include heading-3;
+        margin-bottom: 16px;
+    }
+
+    p {
+        font-size: 15px;
+        line-height: 24px;
+        text-align: left;
+        margin-left: 16px;
+        margin-top: 0;
+    }
+
+    .light-card {
+        background: $white;
+        box-shadow: 0 20px 40px rgba(0, 0, 0, 0.15);
+
+        &:hover {
+            box-shadow: 0 30px 60px rgba(0, 0, 0, 0.35);
+        }
+    }
+
+    .dark-card {
+        background: $super-dark-blue;
+        box-shadow: 0 20px 40px rgba(225, 225, 225, 0.15);
+
+        &:hover {
+            box-shadow: 0 30px 60px rgba(255, 255, 255, 0.35);
+        }
+
     }
 
 </style>
