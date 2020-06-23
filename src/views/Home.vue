633111098939cd64a0284892fc7e21039c4232cb
lm <super1742720898@gmail.com>
2020-06-23 15:55:42 +0800

    :fu: add toggle to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="home">
         <Header/>
+        <div class="btn">
+            <ThemeSwitch/>
+        </div>
         <div class="container">
             <Toggle/>
             <Chart/>
@@ -13,10 +16,12 @@
     import Header from "../components/Header";
     import Chart from "../components/Chart";
     import Toggle from "../components/Toggle";
+    import ThemeSwitch from "../components/ThemeSwitch";
 
     export default {
         name: "Home",
         components: {
+            ThemeSwitch,
             Header,
             Chart,
             Toggle
@@ -27,5 +32,9 @@
     .container {
         padding-left: 15%;
         padding-right: 15%;
+    }
+    .btn{
+        position: absolute;
+        margin-left: 840px;
     }
 </style>
